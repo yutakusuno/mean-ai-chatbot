61ccfd85a21d1c6ce6b5cec6175c89678ced9bc0
Yuta Kusuno <yutakusuno51@gmail.com>
2023-11-12 13:31:03 -0800

    feat: create jwt token and set cookie

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -2,6 +2,7 @@ import express from "express";
 import { config } from "dotenv";
 import morgan from "morgan";
 import appRouter from "./routes/index.js";
+import cookieParser from "cookie-parser";
 
 config();
 
@@ -9,8 +10,9 @@ const app = express();
 
 // middlewares
 app.use(express.json());
+app.use(cookieParser(process.env.COOKIE_SECRET));
 
-// remove it production
+// remove it in production
 app.use(morgan("dev"));
 
 app.use("/api/v1", appRouter);
--- backend/src/controllers/user-controller.ts
+++ backend/src/controllers/user-controller.ts
@@ -1,6 +1,8 @@
 import { NextFunction, Request, Response } from "express";
 import User from "../models/User.js";
 import { compare, hash } from "bcrypt";
+import { createToken } from "../utils/token-manager.js";
+import { COOKIE_NAME } from "../utils/constants.js";
 
 export const getAllUsers = async (
   req: Request,
@@ -25,9 +27,30 @@ export const userSignup = async (
     const { name, email, password } = req.body;
     const existingUser = await User.findOne({ email });
     if (existingUser) return res.status(401).send("User exists");
+
     const hashedPassword = await hash(password, 10);
     const user = new User({ name, email, password: hashedPassword });
     await user.save();
+
+    // create token and set cookie
+    res.clearCookie(COOKIE_NAME, {
+      path: "/",
+      domain: "localhost",
+      httpOnly: true,
+      signed: true,
+    });
+
+    const token = createToken(user._id.toString(), user.email, "7d");
+    const expires = new Date();
+    expires.setDate(expires.getDate() + 7);
+    res.cookie(COOKIE_NAME, token, {
+      path: "/",
+      domain: "localhost",
+      expires,
+      httpOnly: true,
+      signed: true,
+    });
+
     return res.status(201).json({ message: "OK", id: user._id.toString() });
   } catch (error) {
     console.log(error);
@@ -43,9 +66,31 @@ export const userLogin = async (
   try {
     const { email, password } = req.body;
     const user = await User.findOne({ email });
+
     if (!user) return res.status(401).send("User not found");
+
     const isPasswordCorrect = await compare(password, user.password);
     if (!isPasswordCorrect) return res.status(403).send("Wrong password");
+
+    // create token and set cookie
+    res.clearCookie(COOKIE_NAME, {
+      path: "/",
+      domain: "localhost",
+      httpOnly: true,
+      signed: true,
+    });
+
+    const token = createToken(user._id.toString(), user.email, "7d");
+    const expires = new Date();
+    expires.setDate(expires.getDate() + 7);
+    res.cookie(COOKIE_NAME, token, {
+      path: "/",
+      domain: "localhost",
+      expires,
+      httpOnly: true,
+      signed: true,
+    });
+
     return res.status(200).json({ message: "OK", id: user._id.toString() });
   } catch (error) {
     console.log(error);
